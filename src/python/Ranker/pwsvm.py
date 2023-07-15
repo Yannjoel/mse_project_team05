7e4d3e3e41f3d56ae972478f21482eba7ce8ca43
H-Kugler <harald.kugler@student.uni-tuebingen.de>
2023-07-15 16:38:09 +0200

    Pairwise SVM

---
+++ src/python/Ranker/pwsvm.py
@@ -0,0 +1,90 @@
+import numpy as np
+import pandas as pd
+from sklearn import svm, linear_model
+import itertools
+from joblib import dump, load
+
+
+def transform_pairwise(X, y):
+    X_new = []
+    y_new = []
+    y = np.asarray(y)
+    if y.ndim == 1:
+        y = np.c_[y, np.ones(y.shape[0])]
+    comb = itertools.combinations(range(X.shape[0]), 2)
+    for k, (i, j) in enumerate(comb):
+        if y[i, 0] == y[j, 0] or y[i, 1] != y[j, 1]:
+            # skip if same target or different group
+            continue
+        X_new.append(X[i] - X[j])
+        y_new.append(np.sign(y[i, 0] - y[j, 0]))
+        # output balanced classes
+        if y_new[-1] != (-1) ** k:
+            y_new[-1] = - y_new[-1]
+            X_new[-1] = - X_new[-1]
+    return np.asarray(X_new), np.asarray(y_new).ravel()
+
+
+class RankSVM(svm.LinearSVC):
+
+    def fit(self, X, y):
+        """
+        Fit a pairwise ranking model.
+        Parameters
+        ----------
+        X : array, shape (n_samples, n_features)
+        y : array, shape (n_samples,) or (n_samples, 2)
+        Returns
+        -------
+        self
+        """
+        X_trans, y_trans = transform_pairwise(X, y)
+        super(RankSVM, self).fit(X_trans, y_trans)
+        return self
+
+    def predict(self, X):
+        """
+        Predict an ordering on X. For a list of n samples, this method
+        returns a list from 0 to n-1 with the relative order of the rows of X.
+        Parameters
+        ----------
+        X : array, shape (n_samples, n_features)
+        Returns
+        -------
+        ord : array, shape (n_samples,)
+            Returns a list of integers representing the relative order of
+            the rows in X.
+        """
+        if hasattr(self, 'coef_'):
+            np.argsort(np.dot(X, self.coef_.T))
+        else:
+            raise ValueError("Must call fit() prior to predict()")
+
+    def score(self, X, y):
+        """
+        Because we transformed into a pairwise problem, chance level is at 0.5
+        """
+        X_trans, y_trans = transform_pairwise(X, y)
+        return np.mean(super(RankSVM, self).predict(X_trans) == y_trans)
+    
+    def save(self, model_path='svm.joblib'):
+        dump(self.model, model_path) 
+        print('model saved in ', model_path)
+
+    def load(self, model_path='svm.joblib'):
+        self.model = load(model_path)
+        return self.model
+
+
+if __name__ == '__main__':
+    # load training and test set
+    train_data = pd.read_csv('MSLR-WEB10K/Fold1/train.txt', delimiter=" ", nrows=500, header=None)
+    # drop last column and second colum since they are not needed
+    train_data.drop(columns=[1, 138], inplace=True)
+    train_data.iloc[:, 1:] = train_data.iloc[:, 1:].applymap(lambda x: x.split(":", 1)[-1])
+    y_train = train_data[0].astype(float)
+    X_train = train_data.drop(columns=0).values.astype(float)
+
+    rank_svm = RankSVM().fit(X_train, y_train)
+    print('Model fitted')
+    print(rank_svm.score(X_train, y_train))

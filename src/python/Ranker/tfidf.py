c3c7994c89d2dc8b2582a2ea3aa7f13c9d854f4f
H-Kugler <harald.kugler@student.uni-tuebingen.de>
2023-07-16 13:16:08 +0200

    get_idf + similarity

--- src/python/Ranker/tfidf.py
+++ src/python/Ranker/tfidf.py
@@ -1,14 +1,32 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
-from DataHandling.reader import Reader
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 
 class TfIdf:
 
-    def __init__(self):
-        self.reader = Reader()
-        self.corpus = self.reader.get_corpus()
+    def __init__(self, corpus):
+        self.corpus = corpus
+        self.vectorizer = TfidfVectorizer()
+        self.vectorizer.fit(self.corpus)
+    
+    def get_tfidf(self, query):
+        return self.vectorizer.transform([query])
+    
+    def get_tfidf_scores(self, query):
+        return self.vectorizer.transform([query]).toarray()[0]
+    
+    def get_idf(self, query):
+        query_idx = [self.vectorizer.vocabulary_.get(q_i) for q_i in query.split()]
+        if None in query_idx:
+            query_idx.remove(None)
+        if query_idx:
+            return np.mean(self.vectorizer.idf_[query_idx])
+        else:
+            return 0
+        
 
-
-    def get_feature_vecs(self):
-        vectorizer = TfidfVectorizer()
-        X = vectorizer.fit_transform(self.corpus)
-        return X.toarray()
+    def get_similarity_scores(self, query):
+        query_vector = self.vectorizer.transform([query])
+        doc_vectors = self.vectorizer.transform(self.corpus)
+        scores = cosine_similarity(query_vector, doc_vectors)[0]
+        return scores

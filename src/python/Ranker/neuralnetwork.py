251adbef23fdf752522bc56117e3d95768994472
H-Kugler <harald.kugler@student.uni-tuebingen.de>
2023-07-18 14:43:21 +0200

    train-loop

--- src/python/Ranker/neuralnetwork.py
+++ src/python/Ranker/neuralnetwork.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
 
 
 class NeuralNetwork(nn.Module):
@@ -16,13 +17,50 @@ class NeuralNetwork(nn.Module):
         x = self.fc2(x)
         return x
     
-    def save(self, model_path='nn.joblib'):
-        torch.save(self.state_dict(), model_path)
+    def save(self, model_path='model.pth'):
+        torch.save(self, model_path)
         print('model saved in ', model_path)
 
-    def load(self, model_path='nn.joblib'):
-        self.load_state_dict(torch.load(model_path))
-        return self.model
+    @staticmethod
+    def load(model_path='model.pth'):
+        return torch.load(model_path)
     
-    def fit(self, X, y):
-        pass
+    def train_loop(self, data, targets, loss_fn, optimizer):
+        # create dataloader
+        data = TensorDataset(data, targets)
+        dataloader = DataLoader(data, batch_size=32, shuffle=True)
+        size = len(dataloader.dataset)
+        # train
+        self.train()
+        for batch, (X, y) in enumerate(dataloader):
+
+            output = self.get_scores(X)
+            loss = loss_fn(output, y)
+            
+            if batch % 500 == 0:
+                loss_val, current = loss.item(), (batch + 1) * len(X)
+                print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+        return loss.item()
+
+    def test_loop(self, data, targets, loss):
+        # create dataloader
+        data = TensorDataset(data, targets)
+        dataloader = DataLoader(data, batch_size=32, shuffle=True)
+        # test
+        self.eval()
+        test_loss = 0
+        with torch.no_grad():
+            for X, y in dataloader:
+                output = self.get_scores(X)
+                test_loss += loss(output, y)
+
+        test_loss /= len(dataloader)
+        print(f"Test Error: Avg loss: {test_loss:>8f} \n")
+
+
+

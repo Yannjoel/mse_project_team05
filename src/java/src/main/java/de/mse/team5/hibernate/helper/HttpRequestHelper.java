cd2f37a994971d5bf5a42433425c4f04f5a60724
yannick <yannj96@googlemail.com>
2023-07-18 18:46:21 +0200

    Added retries for failed connections

--- src/java/src/main/java/de/mse/team5/hibernate/helper/HttpRequestHelper.java
+++ src/java/src/main/java/de/mse/team5/hibernate/helper/HttpRequestHelper.java
@@ -3,29 +3,45 @@ package de.mse.team5.hibernate.helper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jsoup.Connection;
-import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
+import java.net.NoRouteToHostException;
+import java.time.Duration;
 
 public class HttpRequestHelper {
     private static final Logger LOG = LogManager.getLogger(HttpRequestHelper.class);
+    private static final int MAX_CONNECTION_RETRIES = 15;
 
-    public static Document downloadWebsiteContentForUrl(String url) {
+    public static Document downloadWebsiteContentForUrl(String url, Duration waitTime) {
         Document doc = null;
         try {
-            //Connecting to the web page
-            Connection conn = Jsoup.connect(url);
-            conn.ignoreContentType(true);
-            conn.ignoreHttpErrors(true);
-            conn.header("Accept-Language", "en");
-            //executing the get request
-            doc = conn.get();
-        } catch (HttpStatusException e) {
-            LOG.info(e.getMessage());
-        } catch (IOException e) {
-            LOG.warn("Website " + url + " not available due to " + e.getMessage());
+            int tries = 1;
+            do {
+                try {
+                    //Connecting to the web page
+                    Connection conn = Jsoup.connect(url);
+                    conn.ignoreContentType(true);
+                    conn.ignoreHttpErrors(true);
+                    conn.followRedirects(true);
+                    conn.header("Accept-Language", "en");
+                    //executing the get request
+                    Connection.Response response = conn.execute();
+                    if (response.statusCode() == 200) {
+                        doc = response.parse();
+                    } else {
+                        LOG.debug("Got HTTP STATUS " + response.statusCode() + " for " + url);
+                    }
+                    break;
+                } catch (NoRouteToHostException e) {
+                    tries += 1;
+                    Thread.sleep(waitTime);
+                }
+                LOG.warn("Failed to find route to host " + url + " retry count=" + tries);
+            } while (tries < MAX_CONNECTION_RETRIES);
+        } catch (IOException | IllegalArgumentException | InterruptedException e) {
+            LOG.info("Website " + url + " not available due to " + e.getMessage());
         }
         return doc;
     }

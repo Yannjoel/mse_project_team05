1f62072057a1a1c3111fe6a73bbd63d1d63f9219
yannick <yannj96@googlemail.com>
2023-07-21 00:10:36 +0200

    Removed no longer required restricted test mode

--- src/java/src/main/java/de/mse/team5/crawler/CrawlerFilters.java
+++ src/java/src/main/java/de/mse/team5/crawler/CrawlerFilters.java
@@ -1,38 +1,15 @@
 package de.mse.team5.crawler;
 
 import org.apache.commons.lang3.StringUtils;
-
-import java.util.Arrays;
-import java.util.Collection;
-
 public class CrawlerFilters {
 
-
-    //If enabled the crawler won't touch any other sites that it discovers and will only crawl the website mentioned in the entryUrls
-    public static final Boolean siteRestrictedTestMode = Boolean.FALSE;
-
-    public static final Collection<String> allowedUrls = Arrays.asList("https://www.tuebingen.de/", "https://yannick-huber.de", "https://tue.schulamt-bw.de/Startseite", "http://www.schulamt-tuebingen.de/", "https://www.figurentheater-tuebingen.de", "https://www.landestheater-tuebingen.de/", "https://www.zimmertheater-tuebingen.de/", "http://www.harlekintheater.de/", "http://www.theater-teo-tiger.de/", "https://www.vorstadttheater.de/");
+    private CrawlerFilters(){
+        //prevent initialization
+    }
 
     private static final String[] URL_ENDINGS_TO_IGNORE = new String[]{".pdf", ".png", ".jpg", ".jpeg", ".mp3", ".xls 2"};
 
     public static boolean fitsCrawlFilter(String url) {
-        boolean fitsFilter = true;
-
-        //test filter: check if site is in allowed List
-        if (siteRestrictedTestMode) {
-            boolean isInAllowedUrls = false;
-            for (String allowedUrl : allowedUrls) {
-                if (StringUtils.startsWith(url, allowedUrl)) {
-                    isInAllowedUrls = true;
-                    break;
-                }
-            }
-            fitsFilter = isInAllowedUrls;
-        }
-
-        if (StringUtils.endsWithAny(url, URL_ENDINGS_TO_IGNORE)) {
-            fitsFilter = false;
-        }
-        return fitsFilter;
+        return !StringUtils.endsWithAny(url, URL_ENDINGS_TO_IGNORE);
     }
 }

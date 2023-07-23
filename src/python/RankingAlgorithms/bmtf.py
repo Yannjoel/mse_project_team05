43ba007e54cdff354d8f66883fe4d2b1b2a925d1
Anna <anna@vollweiter.de>
2023-07-23 23:26:55 +0200

    FINALL

--- src/python/RankingAlgorithms/bmtf.py
+++ src/python/RankingAlgorithms/bmtf.py
@@ -8,51 +8,51 @@ class BM25(Ranker):
         self.b = b
         self.is_ranker = is_ranker
 
-    # def get_scores(self, query, df):
-    #     """returns bm25 of doc"""
-    #     if self.is_ranker:
-    #         docs = df["body"]
-    #     else:
-    #         docs = df
-    #     avgdl = np.mean([len(doc.split()) for doc in docs])
-    #     N = len(docs)
-    #     # number of docs containing query term
-    #     n_q = np.array(
-    #         [sum([1 for doc in docs if q_i in doc]) for q_i in query.split()]
-    #     )
-    #     idf = np.log((N - n_q + 0.5) / (n_q + 0.5) + 1)
-    #     # frequency of query term in doc
-    #     f_q_d = np.array(
-    #         [doc.split().count(q_i) for q_i in query.split() for doc in docs]
-    #     )
-    #     f_q_d = f_q_d.reshape(len(query.split()), len(docs))
-    #     doc_len = np.array([len(doc.split()) for doc in docs])
-
-    #     frac = (f_q_d * (self.k + 1)) / (
-    #         f_q_d + self.k * (1 - self.b + self.b * doc_len / avgdl)
-    #     )
-    #     scores = idf @ frac
-
-    #     return scores
-    
     def get_scores(self, query, df):
+        """returns bm25 of doc"""
         if self.is_ranker:
             docs = df["body"]
         else:
             docs = df
         avgdl = np.mean([len(doc.split()) for doc in docs])
         N = len(docs)
-        query = np.array(query.split())
-        n_q = np.array([docs.str.contains(q).sum() for q in query])
+        # number of docs containing query term
+        n_q = np.array(
+            [sum([1 for doc in docs if q_i in doc]) for q_i in query.split()]
+        )
         idf = np.log((N - n_q + 0.5) / (n_q + 0.5) + 1)
+        # frequency of query term in doc
+        f_q_d = np.array(
+            [doc.split().count(q_i) for q_i in query.split() for doc in docs]
+        )
+        f_q_d = f_q_d.reshape(len(query.split()), len(docs))
+        doc_len = np.array([len(doc.split()) for doc in docs])
 
-        f_q_d = np.array([docs.apply(lambda x: x.count(q)).values for q in query])
-        doc_len = docs.apply(lambda x: len(x.split())).values
         frac = (f_q_d * (self.k + 1)) / (
             f_q_d + self.k * (1 - self.b + self.b * doc_len / avgdl)
         )
         scores = idf @ frac
 
         return scores
+    
+    # def get_scores(self, query, df):
+    #     if self.is_ranker:
+    #         docs = df["body"]
+    #     else:
+    #         docs = df
+    #     avgdl = np.mean([len(doc.split()) for doc in docs])
+    #     N = len(docs)
+    #     query = np.array(query.split())
+    #     n_q = np.array([docs.str.contains(q).sum() for q in query])
+    #     idf = np.log((N - n_q + 0.5) / (n_q + 0.5) + 1)
+
+    #     f_q_d = np.array([docs.apply(lambda x: x.count(q)).values for q in query])
+    #     doc_len = docs.apply(lambda x: len(x.split())).values
+    #     frac = (f_q_d * (self.k + 1)) / (
+    #         f_q_d + self.k * (1 - self.b + self.b * doc_len / avgdl)
+    #     )
+    #     scores = idf @ frac
+
+    #     return scores
         
 
